ebb3e6a15fe86cd83c40d6354b570cc0d37656cd
Ian Olson <me@ianolson.io>
2015-08-16 15:43:27 -0500

    Added command to service provider and publishing of the meta data migration file.

---
+++ src/Database/Migrations/2015_08_10_020202_create_meta_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMetaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meta_data', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('method');
+            $table->text('value');
+            $table->integer('reference_id');
+            $table->string('reference_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('meta_data');
+    }
+}
--- src/Providers/SupportServiceProvider.php
+++ src/Providers/SupportServiceProvider.php
@@ -14,13 +14,41 @@ use Illuminate\Support\ServiceProvider;
 
 class SupportServiceProvider extends ServiceProvider
 {
+    /**
+     * {@inheritDoc}
+     */
     public function boot()
     {
+        // Publish migrations.
+        $migrations = realpath(__DIR__ . '/../Database/Migrations');
 
+        $this->publishes([
+          $migrations => database_path('/migrations'),
+        ], 'migrations');
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function register()
     {
+        $this->setupConsoleCommands();
+    }
 
+    /**
+     * Register the console commands to the application.
+     *
+     * Register the following commands:
+     * - iolson:model
+     */
+    protected function setupConsoleCommands()
+    {
+        // Share iolson:model command with the application.
+        $this->app['iolson::model'] = $this->app->share(function () {
+            return new GenerateModel();
+        });
+
+        // Adds iolson:model to the console kernel.
+        $this->commands('iolson::model');
     }
 }
