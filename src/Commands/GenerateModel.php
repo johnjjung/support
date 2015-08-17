c4b25ff87956e945fefd59da14eb548b746b58e6
Ian Olson <me@ianolson.io>
2015-08-16 20:04:06 -0500

    Updated command to include creating the directories and removed extra curly braces on the Repo stub file.

--- src/Commands/GenerateModel.php
+++ src/Commands/GenerateModel.php
@@ -36,6 +36,13 @@ class GenerateModel extends Command
     protected $classname;
 
     /**
+     * Application path.
+     *
+     * @var string
+     */
+    protected $path;
+
+    /**
      * Execute the console command.
      *
      * @return mixed
@@ -44,6 +51,12 @@ class GenerateModel extends Command
     {
         // Grab classname from the console command.
         $this->classname = $this->argument('classname');
+
+        // Application path.
+        $this->path = app_path();
+
+        // Setup files.
+        $this->setUpFiles();
 
         // Output success message.
         $this->info("{$this->classname} model, exception, repo and interface generated.");
@@ -58,8 +71,15 @@ class GenerateModel extends Command
         $contract = __DIR__ . '/stubs/Contract.stub';
         $contractContents = str_replace('{{classname}}', $this->classname, $this->laravel['files']->get($contract));
 
+        // Create contracts directory.
+        try {
+            $this->laravel['files']->makeDirectory("{$this->path}/Contracts");
+        } catch (\ErrorException $e) {
+            // Directory already exists, don't need to recreate.
+        }
+
         // Write new file.
-        if ($this->laravel['files']->put(($contractFile = "{$this->laravel['path.app']}/Contracts/{$this->classname}.php"),
+        if ($this->laravel['files']->put(($contractFile = "{$this->path}/Contracts/{$this->classname}Interface.php"),
             $contractContents) === false
         ) {
             throw new RuntimeException("Could not write env file to [$contractFile].");
@@ -70,7 +90,7 @@ class GenerateModel extends Command
         $exceptionContents = str_replace('{{classname}}', $this->classname, $this->laravel['files']->get($exception));
 
         // Write new file.
-        if ($this->laravel['files']->put(($exceptionFile = "{$this->laravel['path.app']}/Exceptions/{$this->classname}.php"),
+        if ($this->laravel['files']->put(($exceptionFile = "{$this->path}/Exceptions/{$this->classname}Exception.php"),
             $exceptionContents) === false
         ) {
             throw new RuntimeException("Could not write env file to [$exceptionFile].");
@@ -80,8 +100,15 @@ class GenerateModel extends Command
         $model = __DIR__ . '/stubs/Model.stub';
         $modelContents = str_replace('{{classname}}', $this->classname, $this->laravel['files']->get($model));
 
+        // Create models directory.
+        try {
+            $this->laravel['files']->makeDirectory("{$this->path}/Models");
+        } catch (\ErrorException $e) {
+            // Directory already exists, don't need to recreate.
+        }
+
         // Write new file.
-        if ($this->laravel['files']->put(($modelFile = "{$this->laravel['path.app']}/Models/{$this->classname}.php"),
+        if ($this->laravel['files']->put(($modelFile = "{$this->path}/Models/{$this->classname}.php"),
             $modelContents) === false
         ) {
             throw new RuntimeException("Could not write env file to [$modelFile].");
@@ -91,8 +118,15 @@ class GenerateModel extends Command
         $repo = __DIR__ . '/stubs/Repo.stub';
         $repoContents = str_replace('{{classname}}', $this->classname, $this->laravel['files']->get($repo));
 
+        // Create repositories directory.
+        try {
+            $this->laravel['files']->makeDirectory("{$this->path}/Repositories");
+        } catch (\ErrorException $e) {
+            // Directory already exists, don't need to recreate.
+        }
+
         // Write new file.
-        if ($this->laravel['files']->put(($repoFile = "{$this->laravel['path.app']}/Repositories/{$this->classname}.php"),
+        if ($this->laravel['files']->put(($repoFile = "{$this->path}/Repositories/{$this->classname}Repo.php"),
             $repoContents) === false
         ) {
             throw new RuntimeException("Could not write env file to [$repoFile].");
